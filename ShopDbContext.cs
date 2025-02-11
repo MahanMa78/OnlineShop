b95b7716dbbb1d4a8b5fe79d76875f186d92d96a
Mahan Mazaheri <mahan78ir@gmail.com>
2025-02-11 15:57:34 +0330

    Inherite ShopDbContext from IdentityDbContext

--- ShopDbContext.cs
+++ ShopDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineShop.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 namespace OnlineShop;
 
 
-public class ShopDbContext : DbContext
+public class ShopDbContext : IdentityDbContext
 {
     public ShopDbContext(DbContextOptions<ShopDbContext> options)
         : base(options)
@@ -22,6 +24,9 @@ public class ShopDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(x => new { x.LoginProvider, x.ProviderKey });
+        modelBuilder.Entity<IdentityUserRole<string>>().HasKey(x => new { x.UserId, x.RoleId });
+        modelBuilder.Entity<IdentityUserToken<string>>().HasKey(x => new { x.UserId, x.LoginProvider, x.Name });
     }
 }
