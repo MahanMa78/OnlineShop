1ff28ad45fcd3cf80915bf95539ffe5712a3afa2
Mahan Mazaheri <mahan78ir@gmail.com>
2025-03-02 20:45:51 +0330

    Creatre ateUpdateCategoryCommand.cs

---
+++ DTOs/CreateUpdateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.DTOs
+{
+    public class CreateUpdateCategoryCommand
+    {
+        public string Title { get; set; } = default!;
+        public string? Description { get; set; }
+    }
+}
